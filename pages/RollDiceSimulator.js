5eef25114cfa86c4a78a2a0a91dd4248892b9559
Venedictto <mariano.venesio@tiarg.com>
2020-03-29 16:16:50 -0300

    add roll dice  simulator empty page

---
+++ pages/RollDiceSimulator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+const RollDiceSimulator = () => {
+
+
+    return (
+        <div>
+            Roll dice simulator
+        </div>
+    )
+}
+
+
+export default RollDiceSimulator;
