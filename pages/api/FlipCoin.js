d0752848cd59e66f8df4b2aa48901be4742f4da2
Venedictto <mariano.venesio@tiarg.com>
2020-03-31 16:16:35 -0300

    add method to get bigger size of experiments

--- pages/api/FlipCoin.js
+++ pages/api/FlipCoin.js
@@ -3,11 +3,38 @@ import {getSample} from './utils/utils';
 
 export default (req, res) => {
     const size = req.query.size;
-    if(size === undefined) res.status(400).json('Size is not specify');
-    const sample = getSample(parseInt(size), 0, 1, false);
-    const data = [
-        ['Sides of the coin', 'Number of successes', { role: 'style' }],
-        ['Head', sample.filter(value => value === 0).length, ''], 
-        ['Tail', sample.filter(value => value === 1).length, '']]
+    const p = req.query.p;
+    if(size === undefined || p === undefined) res.status(400).json('Size is not specify');
+    const data = getFlipCoinResults(parseInt(size), 0, 1, p)
     res.status(200).json(data);
+}
+
+
+const getFlipCoinResults = (size, min, max, p) => {
+    let heads = 0;
+    let tails = 0;
+    let haveExperiments = true;
+    let repetitions = size;
+    while (haveExperiments){
+        let sample = [];
+        if(repetitions > 1000000)
+        {
+            sample = getSample(1000000, min, max, true);
+            repetitions -= 1000000
+        } 
+        else 
+        {
+            sample = getSample(repetitions, min, max, true);
+            haveExperiments = false;
+        }
+        heads += sample.filter(value => value > p).length;
+        tails += sample.filter(value => value < p).length;
+    }
+    const data = 
+       [['Sides of the coin', 'Number of successes', { role: 'style' }],
+        ['Head', heads, ''], 
+        ['Tail', tails, '']]
+    
+    return data;
+
 }
