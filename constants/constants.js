89b54a1c95732129564a17b091fb8b15d0973f77
Venedictto <mariano.venesio@tiarg.com>
2020-03-31 16:15:54 -0300

    add random loader tyle

---
+++ constants/constants.js
@@ -0,0 +1,11 @@
+export const loaderTypes = [
+                            'Audio',
+                            'Ball-Triangle0',
+                            'Bars',
+                            'Circles',
+                            'Grid',
+                            'Oval',
+                            'Puff',
+                            'Rings',
+                            'TailSpin',
+                            'ThreeDots'];
--- pages/api/utils/utils.js
+++ pages/api/utils/utils.js
@@ -1,6 +1,6 @@
 import R from 'ramda';
-
-const getRandomNumber = (min, max, isFloat) => {
+import {loaderTypes} from '../../../constants/constants';
+export const getRandomNumber = (min, max, isFloat) => {
     let range = max - min
     let random = Math.random() * range + min
     const number = isFloat ? random : Math.round(random);
@@ -8,7 +8,10 @@ const getRandomNumber = (min, max, isFloat) => {
   }
 
 export const getSample = (size, min, max, areFloat) => {
-    return  new Array(size).fill(0).map( () =>  getRandomNumber(min, max, false));
+    return  new Array(size).fill(0).map( () => getRandomNumber(min, max, areFloat));
 }
 
+export const getRandomLoaderType = () => {
+  return loaderTypes[getRandomNumber(0, loaderTypes.length-1, false)]
+}
 
