0cbd4d167f9d3a165fa8b9457c6d12fe1639d1ca
Venedictto <mariano.venesio@tiarg.com>
2020-04-01 11:46:53 -0300

    add flip coint until page

--- pages/FlipCoinUntilSimulator.js
+++ pages/FlipCoinUntilSimulator.js
@@ -1,14 +1,147 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import Card from '../Components/Card/Card';
+import styled from 'styled-components';
+import Input from '../Components/Input/Input';
+import Button from '../Components/Button/Button';
+import Chart from 'react-google-charts';
+import fetch from 'isomorphic-unfetch';
+import Loader from 'react-loader-spinner';
+import {getRandomLoaderType} from './api/utils/utils';
 
-
-
+const VariableContainer = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content:center;
+    flex-wrap:wrap;
+`;
+const InputContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    margin:1rem;
+    font-family: ${props => props.theme.font.family};
+    font-size: ${props => props.theme.font.size.text};
+    font-weight: ${props => props.theme.font.weight.bold};
+`;
+const ChartContainer = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+`;
+const ErrorField = styled.div`
+    color: red;
+    border-radius:10px;
+    height: 2rem;
+    font-weight: ${props => props.theme.font.weight.bold};
+`;
+const NumberOfExperiments = styled(Input).attrs({placeholder:'1-1000000', type:'number', name:'Experiments'})`
+`;
+const HeadProbability = styled(Input).attrs({placeholder:'0-1', type:'number', name:'Probability'})`
+`;
+const chartOptions = {
+    title: 'omega',
+    chartArea: { width: '50%' },
+    hAxis: {
+        title: 'Number of success',
+        minValue: 0,
+    },
+    vAxis: {
+        title: 'Omega',
+    },
+    bar: { groupWidth: '95%' },
+    legend: { position: 'none' },
+};
 
 const FlipCoinUntilSimulator = () => {
 
+    const [Experiments, setExperiments] = useState('2000');
+    const [Probability, setProbability] = useState('0.50');
+    const [FieldError, setFieldError] = useState('');
+    const [Loading, setLoading] = useState(false);
+    const [ExperimentData, setExperimentData] = useState(undefined)
+
+    const inputRangeValidator = useCallback(
+        (event,setFuncion) => {
+            let { value, min, max } = event.target;
+            if (value === '' || Math.max(Number(min), Math.min(Number(max), Number(value))) !== Number(value)){
+              setFuncion('');
+              setFieldError(`Number of experiments allowed between ${min} and ${max}`);
+            }
+            else{
+              setFieldError('');
+              setFuncion(value);
+            }
+        }, []
+    );    
+    const getExperimentResults = useCallback(
+        (Experiments,Probability) => {
+            setLoading(true);
+            setExperimentData(undefined);
+            const url =`api/FlipCoinUntil?size=${Experiments}&p=${Probability}` 
+            fetch(url)
+            .then(resolve => resolve.json())
+            .then(data => {setLoading(false); setExperimentData(data)})
+        }, []
+    );   
 
     return (
         <div>
-            FlipCoinUntilSimulator
+            <Card>
+                <VariableContainer>
+                    <InputContainer>
+                        <NumberOfExperiments
+                                value={Experiments}
+                                onBlur={(event)=> inputRangeValidator(event,setExperiments)}
+                                onChange={(event) => {setExperiments(event.target.value)}}
+                                name="Experiments"
+                                min="1"
+                                max="10000000">
+                        </NumberOfExperiments>
+                        <label>Experiments</label>
+                    </InputContainer>
+                    <InputContainer>
+                        <HeadProbability
+                                value={Probability}
+                                onBlur={(event)=> inputRangeValidator(event,setProbability)}
+                                onChange={(event) => {setProbability(event.target.value)}}
+                                name="Probability"
+                                min="0.1"
+                                max="1"
+                            />
+                        <label>Probability</label>
+                    </InputContainer>
+                    <InputContainer>
+                        <Button onClick={() => getExperimentResults(Experiments, Probability)} disabled={Loading}>Go!</Button>
+                    </InputContainer>
+                </VariableContainer>
+                {
+                    FieldError !== '' ? <ErrorField> ** {FieldError} </ErrorField> : <></>
+                }
+                <ChartContainer>
+                    {
+                        Loading &&
+                            <Loader
+                                // @ts-ignore
+                                type={getRandomLoaderType()}
+                                color="#455a64"
+                                height={250}
+                                width={250}
+                                timeout={5000}/>
+                    }
+                    {
+
+                        ExperimentData !== undefined &&
+                            <Chart
+                                width={'500px'}
+                                height={'800px'}
+                                chartType="BarChart"
+                                loader={<div>Loading Chart</div>}
+                                data={ExperimentData}
+                                options={chartOptions}
+                            />
+                    }
+                    
+                </ChartContainer>
+            </Card>
         </div>
     )
 }
