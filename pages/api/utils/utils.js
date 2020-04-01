1de7bad357aedf82c07446ca7fe3c9e54fc7327b
Venedictto <mariano.venesio@tiarg.com>
2020-04-01 11:46:36 -0300

    add get theme colours method

--- pages/api/utils/utils.js
+++ pages/api/utils/utils.js
@@ -1,5 +1,6 @@
 import R from 'ramda';
 import {loaderTypes} from '../../../constants/constants';
+import theme from '../../../constants/Theme';
 
 export const getRandomNumber = (min, max, isFloat) => {
     let range = max - min
@@ -11,3 +12,5 @@ export const getRandomNumber = (min, max, isFloat) => {
 export const getRandomLoaderType = () => {
   return loaderTypes[getRandomNumber(0, loaderTypes.length-1, false)];
 }
+
+export const getThemeColours = () => { return Object.keys(theme.color).map(key => theme.color[key]) }
