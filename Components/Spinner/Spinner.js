e2ee774bae2973af58e8d42a8ffcbc39ad2f5fab
Venedictto <mariano.venesio@tiarg.com>
2020-04-07 15:13:07 -0300

    change loader library

--- Components/Spinner/Spinner.js
+++ Components/Spinner/Spinner.js
@@ -1,29 +1,42 @@
 
-import React from 'react';
+import React, {useCallback} from 'react';
 import styled from 'styled-components';
-import Loader from 'react-loader-spinner';
-import {getRandomLoaderType, getRandomThemeColour} from '../../pages/api/utils/utils';
+import {getRandomThemeColour,getRandomNumber} from '../../pages/api/utils/utils';
 
-const CenterLoader = styled(Loader)`
-    display:flex !important;
-    justify-content:center !important;
+import {ClimbingBoxLoader, ClockLoader, DotLoader, HashLoader, PacmanLoader} from "react-spinners";
+
+const SpinnerContainer = styled.div`
+    position: absolute;
+    right:0px;
+    top:0px;
+    width:100%;
+    height:100%;
+    z-index:100;
+    background-color:rgba(245, 245, 245,0.3);
 `;
-
+const SpinnerWrapper = styled.div`
+    position:relative;    
+    top: 50vh;
+    left: 40vw;
+    @media (max-width: 768px) {
+        left:0px;
+    }
+`;
 const Spinner = (props) => {
     const {loading} = props;
+
     return (
         <>
             {
-            loading &&
-                <CenterLoader
-                    // @ts-ignore
-                    type={getRandomLoaderType()}
-                    color={getRandomThemeColour(1)}
-                    height={250}
-                    width={250}
-                    timeout={10000}/>
+                loading &&
+                <SpinnerContainer>
+                    <SpinnerWrapper>
+                        <PacmanLoader size={90} color={getRandomThemeColour(1)} loading={loading}/>
+                    </SpinnerWrapper>
+                </SpinnerContainer>
             }
         </>
+        
     )
 }
 export default Spinner;
